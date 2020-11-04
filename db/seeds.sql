0b170cef7e04acdc9d1067019d536ae6e78b4a91
Trisha Cole <tee.cole2@gmail.com>
2020-11-04 18:58:54 -0500

    Final Review

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('California Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Guacamole Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Steak Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+
