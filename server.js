de9b80468d5c4d4b343c14d4f871f3bfcdef50f1
Trisha Cole <tee.cole2@gmail.com>
2020-11-22 18:56:39 -0500

    adding server.js code

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// Import routes and give the server access to them.
+let routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
